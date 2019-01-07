6253a397fcc74b22f9dee0bc7c055b2115a58cde
anass benjelloun <abenjell2012@gmail.com>
2019-01-07 01:00:27 +0100

    test

--- src/main/java/com/test/AppMain.java
+++ src/main/java/com/test/AppMain.java
@@ -5,7 +5,7 @@ public class AppMain {
 	public static void main(String[] args) {
 
 		System.out.println("HelloWorld");
-		System.out.println("HelloWorld ");
+		System.out.println("HelloWorld 2");
 
 	}
 
